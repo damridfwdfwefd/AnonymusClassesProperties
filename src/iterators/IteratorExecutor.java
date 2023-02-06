9c94b1674601561e164769a31ef8295fc231b6ff
damridfwdfwefd <alexdamsem2004@gmail.com>
2023-02-06 21:51:34 +0900

    Initial commit

---
+++ src/iterators/IteratorExecutor.java
@@ -0,0 +1,21 @@
+package iterators;
+
+class IteratorExecutor {
+
+    static void performIterationsWithCallback(int numberOfIterations, LoopCallback callback) {
+        for (int i = 0; i < numberOfIterations; i++) {
+            callback.onNewIteration(i);
+        }
+    }
+
+    static void startIterations(int numberOfIterations) {
+       LoopCallback start = new LoopCallback() {
+           @Override
+           public void onNewIteration(int iteration) {
+               System.out.println("Etaration; " + iteration );
+
+           }
+       };
+       performIterationsWithCallback(numberOfIterations,start);
+    }
+}
---
+++ src/iterators/Main.java
@@ -0,0 +1,7 @@
+package iterators;
+
+public class Main {
+    public static void main (String[] args) {
+        IteratorExecutor.startIterations(8);
+    }
+}
